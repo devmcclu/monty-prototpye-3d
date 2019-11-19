03dadc6e353a071d8964f7076098229ffc4509c0
Devlin McClure <mccluredt@gmail.com>
2019-11-19 18:47:10 -0500

    Create GummyFollow script to make gummies follow the player

---
+++ Assets/Scripts/GummyFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GummyFollow : MonoBehaviour
+{
+    public Transform leader;
+    //public float followSharpness = 0.1f;
+    //Vector3 _followOffset;
+    public float speed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_followOffset = transform.position - leader.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, leader.position, speed);
+    }
+    
+    void FixedUpdate()
+    {
+        // Apply that offset to get a target position.
+        //Vector3 targetPosition = leader.position + _followOffset;
+
+        // Keep our y position unchanged.
+        //targetPosition.y = transform.position.y;
+
+        // Smooth follow.    
+        //transform.position += (targetPosition - transform.position) * followSharpness;
+    }
+}
