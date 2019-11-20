46b4850db5b5baad669a16e282633ae396bda2e0
Devlin McClure <mccluredt@gmail.com>
2019-11-19 20:08:39 -0500

    Script for when a gummy walks into an environment trigger

---
+++ Assets/Scripts/EnvironmentCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentCollider : MonoBehaviour
+{
+    //Stop a Gummy from moving once it enters the area
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("What this?");
+        if(other.gameObject.CompareTag("Gummy"))
+        {
+            Debug.Log("GUMMY HIT");
+            other.GetComponent<GummyFollow>().stop = true;
+        }
+    }
+}
