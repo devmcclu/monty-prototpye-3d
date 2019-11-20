edd298a64fd1d1520b7c40b0bc2787df125617bb
Devlin McClure <mccluredt@gmail.com>
2019-11-20 14:40:48 -0500

    Script for when the player runs into a gummy

---
+++ Assets/Scripts/GummyTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GummyTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == GetComponent<GummyFollow>().player.gameObject)
+        {
+            GetComponent<GummyFollow>().AddGummy();
+        }
+    }
+}
