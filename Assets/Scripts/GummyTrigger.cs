9d3e806a41999141c3f3e91ab8670d91c8b835e1
Devlin McClure <mccluredt@gmail.com>
2019-11-20 19:40:33 -0500

    Make Gummy Trigger more efficient

--- Assets/Scripts/GummyTrigger.cs
+++ Assets/Scripts/GummyTrigger.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class GummyTrigger : MonoBehaviour
 {
+    private GummyFollow followScipt;
+
+    void Start()
+    {
+        followScipt = GetComponent<GummyFollow>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == GetComponent<GummyFollow>().player.gameObject)
+        if(other.gameObject == followScipt.player.gameObject)
         {
-            GetComponent<GummyFollow>().AddGummy();
+            followScipt.AddGummy();
         }
     }
 }
