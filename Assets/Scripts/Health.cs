6864218c912d9d78e92c6c35aab9ba47eb44a4e3
Devlin McClure <mccluredt@gmail.com>
2019-12-02 15:15:00 -0500

    Create Health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int health;
+    public int startingHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startingHealth;
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void HealthDamage(int amount)
+    {
+        health -= amount;
+    }
+}
