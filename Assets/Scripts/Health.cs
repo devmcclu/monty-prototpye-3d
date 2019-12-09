47dbf35b6fc06f0a12f2b7b6be39f8b508484f5b
Devlin McClure <mccluredt@gmail.com>
2019-12-08 21:25:37 -0500

    Add comments to many scripts

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-
+    //Current health
     public int health;
+    //Starting health of the object
     public int startingHealth;
+    
     // Start is called before the first frame update
     void Start()
     {
         health = startingHealth;
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
-
     public void HealthDamage(int amount)
     {
+        //Remove the health
         health -= amount;
+        //Destroy the object if the health is at or below 0
         if(health <= 0)
         {
             Destroy(this.gameObject);
--- Assets/Scripts/SlingshotTrigger.cs
+++ Assets/Scripts/SlingshotTrigger.cs
@@ -3,30 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlingshotTrigger : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    
+{   
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("What this?");
+        //If a gummy hits the slingshot
         if(other.gameObject.CompareTag("Gummy"))
         {
             GummyFollow gummy = other.GetComponent<GummyFollow>();
+            //If the gummy isn't following the player
             if(gummy.followPlayer == false)
             {
                 Debug.Log("GUMMY HIT");
                 gummy.stop = true;
+                //Shoot the gummy using physics
                 Rigidbody gummyRb = gummy.GetComponent<Rigidbody>();
                 gummyRb.isKinematic = false;
                 gummyRb.AddForce(new Vector3(0, 250, 250));
