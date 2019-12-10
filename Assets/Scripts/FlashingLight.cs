4f41c263bbf67edfd4fc909502eb003d7aae97fe
Joey Mazero <joemazero337@hotmail.com>
2019-12-10 12:54:49 -0500

    Made activation area

---
+++ Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+
+
+    public Light testLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    void Start()
+    {
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            testLight.enabled = !testLight.enabled;
+
+        }
+    }
+}
