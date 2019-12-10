515a2e61639e4cf174fb1cee0e9c8556d4fe043a
Devlin McClure <mccluredt@gmail.com>
2019-12-10 13:39:04 -0500

    Create trampoline

---
+++ Assets/Scripts/TrampolineTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("What this?");
+        //If a gummy hits the slingshot
+        if(other.gameObject.CompareTag("Player"))
+        {
+            CharacterController playerRb = other.GetComponent<CharacterController>();
+            playerRb.Move(new Vector3(0, 10, 0));
+
+        }
+    }
+}
